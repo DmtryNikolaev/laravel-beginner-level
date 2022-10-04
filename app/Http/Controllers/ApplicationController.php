22dcc10a6dec7d184a14807a29c63d70f5642e37
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-10-04 14:40:51 +0500

    Реализация отправки данных

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ApplicationController extends Controller
+{
+    public function store(Request $request)
+    {   
+        $data = $request->validate([
+            'name' => 'required',
+            'phone' => ['required', 'integer'],
+            'mail' => 'email',
+            'additional_information' => 'string',
+            'clientID' => 'integer',
+            'utm_source' => 'string',
+            'utm_medium' => 'string',
+            'utm_campaign' => 'string',
+            'utm_content' => 'string',
+            'utm_term' => 'string',
+            'source' => 'text'
+        ]);
+
+        $application = new Application();
+        $application->source = $request->header('User-Agent');
+        $application->fill($data);
+        $application->save();
+        
+        return response('Успешно отправлено');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
-    Route::get('/get', 'GetController');
+    Route::post('/peoples_applications', [ApplicationController::class, 'store']);
+    Route::get('/sorted', [ApplicationController::class, 'sorted']); 
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
